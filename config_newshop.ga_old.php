4fffe72c436bfdb5e783d1b0d5d30e68305944b4
Bart <zbartynz@gmail.com>
2018-09-26 12:49:04 +0300

    возврат к началу
    на старте

---
+++ config_newshop.ga_old.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://newshop.ga/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://newshop.ga/');
+
+// DIR
+define('DIR_APPLICATION', '/home/dreamsof/newshop.ga/catalog/');
+define('DIR_SYSTEM', '/home/dreamsof/newshop.ga/system/');
+define('DIR_IMAGE', '/home/dreamsof/newshop.ga/image/');
+define('DIR_LANGUAGE', '/home/dreamsof/newshop.ga/catalog/language/');
+define('DIR_TEMPLATE', '/home/dreamsof/newshop.ga/catalog/view/theme/');
+define('DIR_CONFIG', '/home/dreamsof/newshop.ga/system/config/');
+define('DIR_CACHE', '/home/dreamsof/newshop.ga/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/dreamsof/newshop.ga/system/storage/download/');
+define('DIR_LOGS', '/home/dreamsof/newshop.ga/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/dreamsof/newshop.ga/system/storage/modification/');
+define('DIR_UPLOAD', '/home/dreamsof/newshop.ga/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'dreamsof_newshop');
+define('DB_PASSWORD', 'wpwcLAU2s4lF');
+define('DB_DATABASE', 'dreamsof_newshop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
